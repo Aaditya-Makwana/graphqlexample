b6f8a59e4445d3806959d9190cc33a6a226058a3
Aaditya-Makwana <aadityamakwana2003@gmail.com>
2025-01-15 10:44:57 +0530

    Working graphQL with spqr without schema

--- src/main/java/com/aaditya/GraphQLExampleApplication.java
+++ src/main/java/com/aaditya/GraphQLExampleApplication.java
@@ -1,13 +1,7 @@
 package com.aaditya;
 
-import com.aaditya.model.Author;
-import com.aaditya.model.Book;
-import com.aaditya.repository.AuthorRepository;
-import com.aaditya.repository.BookRepository;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
@@ -15,18 +9,5 @@ import java.util.List;
 public class GraphQLExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(GraphQLExampleApplication.class, args);
-    }
-
-    @Bean
-    ApplicationRunner applicationRunner(AuthorRepository authorRepository, BookRepository bookRepository){
-        return args -> {
-//            Author josh = authorRepository.save(new Author(null, "Josh Long"));
-//            Author mark = authorRepository.save(new Author(null, "Mark Short"));
-//            bookRepository.saveAll(List.of(
-//                    new Book("Long", "LongPub", josh.getId()),
-//                    new Book("Short", "ShortPub", mark.getId()),
-//                    new Book("Spring boot", "JavaPub", josh.getId())
-//            ));
-        };
     }
 }
--- src/main/java/com/aaditya/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aaditya.repository;
-
-import com.aaditya.model.Test;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TestRepository extends MongoRepository<Test, String> {
-}
